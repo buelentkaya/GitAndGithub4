8ad66fa8558faee69216a4b45c3afc9d065580ff
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-14 14:43:12 +0200

    next commit

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -3,5 +3,8 @@ package Hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("EU08 learning git and github");
+
     }
 }
