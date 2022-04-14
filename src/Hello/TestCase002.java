cce7a1b402412034e6a1a18987ec48e20b74af92
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-14 15:06:31 +0200

    testCase 2 Class step 2

--- src/Hello/TestCase002.java
+++ src/Hello/TestCase002.java
@@ -4,5 +4,6 @@ public class TestCase002 {
     public static void main(String[] args) {
 // awesome
         System.out.println(" hello Github");
+        System.out.println(" hello Git");
     }
 }
